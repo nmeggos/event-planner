8612d2f8b1fd5deb2e061a4bd68217b65548bfcf
Nikitas Meggos <nmeggos@visualcomfortco.com>
2024-11-30 11:36:32 -0600

    adding initial variables in program

--- backend/src/Modules/UserManagement/EventPlanner.Modules.UserManagement.API/Program.cs
+++ backend/src/Modules/UserManagement/EventPlanner.Modules.UserManagement.API/Program.cs
@@ -1,4 +1,10 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var environment = builder.Environment;
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
